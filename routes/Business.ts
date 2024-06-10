2f525bafd0c74666d5b37b0e9f153c95046ca274
sungohki <sokim1201@naver.com>
2024-06-10 15:24:08 +0900

    delete: Rename files

--- routes/Business.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-const router = express.Router();
-router.use(express.json());
-
-router.get('/', () => {});
-router.get('/:id', () => {});
-
-module.exports = router;
