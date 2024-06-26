365aec17bf36d24995fb77f1f188d331216a9466
sungohki <sokim1201@naver.com>
2024-06-26 21:30:33 +0900

    feat: Add mariadb connection and function

--- services/user/userJoin.ts
+++ services/user/userJoin.ts
@@ -1,7 +1,49 @@
+// import node module
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import crypto from 'crypto';
+
+// import local module
+import { createRes } from '../common';
+import { connection as conn } from '../../mariadb';
+import { ResultSetHeader } from 'mysql2';
+
+export interface IUserAccount {
+  userName: string;
+  userId: string;
+  userPw: string;
+  userContact: string;
+  salt: string;
+}
 
 export const userJoin = (req: Request, res: Response) => {
-  res.write('회원 가입 요청');
-  return res.status(StatusCodes.OK).end();
+  const { userName, userId, userPw, userContact } = req.body;
+
+  // Info: Hashing password
+  const salt = crypto.randomBytes(64).toString('base64');
+  // randomBytes 메서드를 사용하여 64바이트의 무작위 salt 값을 생성하고 이를 base64 문자열로 변환
+  const hashedPassword = crypto
+    .pbkdf2Sync(userPw, salt, 10000, 64, 'sha512')
+    .toString('base64');
+  /*
+    pbkdf2Sync 메서드를 사용하여 비밀번호를 해싱(암호화)
+    해싱 알고리즘으로 sha512를 사용하고, 해싱된 결과를 base64 문자열로 변환
+  */
+
+  const sql: string = `
+    INSERT INTO 
+      user 
+      (userName, userId, userPw, userContact, salt) 
+    VALUES 
+      (?, ?, ?, ?, ?)`;
+  const values: Array<any> = [
+    userName,
+    userId,
+    hashedPassword,
+    userContact,
+    salt,
+  ];
+  conn.query<ResultSetHeader>(sql, values, (err, results) => {
+    createRes(res, err, results);
+  });
 };
--- services/user/userLogin.ts
+++ services/user/userLogin.ts
@@ -1,7 +1,70 @@
+// import node module
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import crypto from 'crypto';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
+
+// import local module
+import { connection as conn } from '../../mariadb';
+import { IUserAccount } from './userJoin';
+import { RowDataPacket } from 'mysql2';
+
+interface UserQueryResult extends IUserAccount, RowDataPacket {}
 
 export const userLogin = (req: Request, res: Response) => {
-  res.write('로그인 요청');
-  return res.status(StatusCodes.OK).end();
+  const { userId, userPw } = req.body;
+  const sql = `SELECT * FROM user WHERE userId = ?`;
+  const values = [userId];
+
+  conn.query(sql, values, (err, results: UserQueryResult[]) => {
+    if (err) {
+      console.log(err);
+      return res.status(StatusCodes.BAD_REQUEST).end();
+    }
+    const loginUser = results[0];
+    console.log(loginUser);
+
+    // TODO: Checking password
+    const hashedPassword = crypto
+      .pbkdf2Sync(userPw, loginUser.salt, 10000, 64, 'sha512')
+      .toString('base64');
+    console.log(hashedPassword);
+
+    if (loginUser && loginUser.userPw == hashedPassword) {
+      console.log('로그인 정보 일치');
+      // 0) Check if PRIVATE_KEY is defined
+      const privateKey = process.env.PRIVATE_KEY;
+      if (!privateKey) {
+        console.error('PRIVATE_KEY가 환경변수로 지정되어있지 않음.');
+        return res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+      }
+      // 1) jwt 토큰 발행
+      const instanceToken = jwt.sign(
+        {
+          id: loginUser.id,
+          userId: loginUser.userId,
+        },
+        privateKey,
+        {
+          expiresIn: '30m',
+          issuer: 'sungohki',
+        }
+      );
+      console.log('토큰 발행');
+      // 2) 쿠키에 토큰 첨부
+      res.cookie('token', instanceToken, {
+        httpOnly: true,
+      });
+      return res
+        .status(StatusCodes.OK)
+        .json({ ...results[0], token: instanceToken });
+    } else {
+      return res.status(StatusCodes.UNAUTHORIZED);
+    }
+  });
+
+  // res.write('로그인 요청');
+  // return res.status(StatusCodes.OK).end();
 };
