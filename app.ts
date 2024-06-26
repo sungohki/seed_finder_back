aa994ddbc02254417fb05fc09e4a9f154ddfaa31
sungohki <sokim1201@naver.com>
2024-06-26 21:30:43 +0900

    feat: Refactoring code

--- app.ts
+++ app.ts
@@ -7,13 +7,11 @@ import { userRouter } from './routes/userRoutes';
 import { businessRouter } from './routes/businessRoutes';
 
 // routing uri
-// app.get('/', (req: Request, res: Response) => {
-//   return res
-//     .json({
-//       name: 'main page',
-//     })
-//     .end(200);
-// });
+app.get('/', (req: Request, res: Response) => {
+  return res.json({
+    name: 'main page',
+  });
+});
 app.use('/user', userRouter);
 app.use('/business', businessRouter);
 
