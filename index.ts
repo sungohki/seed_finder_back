99cef93a7aee43cb3cec7ff5fbf5dcb0f0f483b3
sungohki <sokim1201@naver.com>
2024-07-17 16:49:05 +0900

    feat: Refactoring for ec2

--- index.ts
+++ index.ts
@@ -6,8 +6,9 @@ dotenv.config();
 
 // Retrieve the port number
 const port = process.env.PORT || 3000;
+const host = process.env.DOMAIN || "localhost";
 
 // Open server with port number
 app.listen(port, () => {
-  console.log(`Server is open at http://localhost:${port}`);
+  console.log(`Server is open at http://${host}:${port}`);
 });
--- mariadb.ts
+++ mariadb.ts
@@ -1,22 +1,8 @@
 import mariadb, { Connection } from 'mysql2';
 
-// // Connection for Localhost
-// export const connection: Connection = mariadb.createConnection({
-//   // host: 'localhost',
-//   host: '127.0.0.1',
-//   port: 3306,
-//   user: 'root',
-//   password: 'root',
-//   database: 'seedfinder',
-//   dateStrings: true,
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
-
-// // Connection for aws ec2
 export const connection: Connection = mariadb.createConnection({
-  host: '127.0.0.1',
+  host: 'localhost',
+  // host: '127.0.0.1',
   port: 3306,
   user: 'root',
   password: 'root',
