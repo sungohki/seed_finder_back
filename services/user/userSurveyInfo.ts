dd2455cec221b9923e5a949617bb83a1b0907772
sungohki <sokim1201@naver.com>
2024-08-14 19:13:37 +0900

    feat: Refactoring

--- services/user/userSurveyInfo.ts
+++ services/user/userSurveyInfo.ts
@@ -2,7 +2,6 @@ import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import { verifyAccessToken } from '../common';
 import { connection as conn } from '../../mariadb';
-import { QueryError } from 'mysql2';
 
 interface ISurveyInfo {}
 
