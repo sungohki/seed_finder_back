d7550774c81e6a4b6b765091779c5cc6ea44c7d9
sungohki <sokim1201@naver.com>
2024-08-15 12:33:41 +0900

    feat: Test swagger

--- routes/userRoutes.ts
+++ routes/userRoutes.ts
@@ -9,6 +9,37 @@ import {
 const router = express.Router();
 router.use(express.json());
 
+/**
+ * @swagger
+ * tags:
+ *   name: User
+ *   description: User management
+ */
+
+/**
+ * @swagger
+ * /user:
+ *   get:
+ *     summary: Retrieve a list of users
+ *     tags: [User]
+ *     responses:
+ *       200:
+ *         description: A list of users
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 type: object
+ *                 properties:
+ *                   id:
+ *                     type: integer
+ *                     example: 1
+ *                   name:
+ *                     type: string
+ *                     example: John Doe
+ */
+
 // Create user login token
 router.post('/login', userLoginHandler);
 // Create user account
