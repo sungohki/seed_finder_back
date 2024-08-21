4a3c1209617e7e27b4e15d682225bfbde0a073a6
sungohki <sungohki@DESKTOP-4QPLQ55>
2024-08-22 01:01:11 +0900

    feat: Update sql phrase

--- services/user/userSurveyInfo.ts
+++ services/user/userSurveyInfo.ts
@@ -5,21 +5,22 @@ import { verifyAccessToken } from '../common';
 import mariadb from 'mysql2/promise';
 import { connInfo } from '../../mariadb';
 
-interface ISurveyInfo {
-  businessCategory: string;
-  businessRegion: string;
-  businessApply: string;
+export interface ISurveyInfo {
+  businessCategory: Array<string>;
+  businessRegion: Array<string>;
+  businessApply: Array<string>;
   businessExperience: string;
   businessTargetAge: number;
 }
 
 // 회원 정보 설문
 export const userSurveyInfo = async (req: Request, res: Response) => {
-  const decodedInfo = verifyAccessToken(req, res);
-  if (decodedInfo === null) return;
-
+  const decodedUserInfo = verifyAccessToken(req, res);
+  if (decodedUserInfo === null)
+    return res.status(StatusCodes.UNAUTHORIZED).json({
+      message: 'info: 토큰 인식 불가',
+    });
   const conn = await mariadb.createConnection(connInfo);
-
   const {
     businessCategory,
     businessRegion,
@@ -27,54 +28,77 @@ export const userSurveyInfo = async (req: Request, res: Response) => {
     businessExperience,
     businessTargetAge,
   } = req.body as ISurveyInfo;
+  let sql, values, results;
 
-  // 1. user 데이터 추가
-  let sql = `
-    INSERT INTO 
-      user
-      (support_business_classification_id, support_region_id)
-    VALUES
-      (?, ?)`;
-  let values = [businessCategory, businessRegion];
-  let [results] = await conn.query(sql, values);
-  console.log(`1. ` + results);
-  if (results) {
-    return res.status(StatusCodes.UNAUTHORIZED).json({
-      message: 'info: 체크포인트1',
-    });
+  // 1. 지원사업분류
+  for(let item in businessCategory) {
+    sql = `
+      INSERT INTO
+        tableName
+          (user_id, support_business_classification_id)
+        VALUE
+          (?, (SELECT id FROM Support_Business_Classification WHERE name = ?))`;
+    values = [decodedUserInfo.id, item];
+    [results] = await conn.query(sql, values);
+    console.log(`1. ` + results);
+    if (results) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        message: 'info: 체크포인트1',
+      });
+    }
   }
+  // 2. 신청대상
+  for(let item in businessApply) {
+    sql = `
+      INSERT INTO
+        tableName
+          (user_id, application_target_id)
+        VALUE
+          (?, (SELECT id FROM Application_Target WHERE name = ?))`;
+    values = [decodedUserInfo.id, item];
+    [results] = await conn.query(sql, values);
+    console.log(`2. ` + results);
+    if (results) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        message: 'info: 체크포인트2',
+      });
+    }
+  }
+  // 3. 지역
+  for(let item in businessRegion) {
+    sql = `
+      INSERT INTO
+        tableName
+          (user_id, support_region_id)
+        VALUE
+          (?, (SELECT id FROM Support_Region WHERE name = ?))`;
+    values = [decodedUserInfo.id, item];
+    [results] = await conn.query(sql, values);
+    console.log(`3. ` + results);
+    if (results) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        message: 'info: 체크포인트3',
+      });
+    }
+  }
+  // 4. 업력 & 예비창업자여부 & 연령
+    sql = `
+        INSERT INTO
+          user
+            (user_age, pre_business_status, business_duration)
+          VALUE
+            (?, ?, ?)`;
+    if (businessExperience === "예비창업자") 
+      values = [businessTargetAge, true, null];
+    else 
+      values = [businessTargetAge, false, businessExperience];
+    [results] = await conn.query(sql, values);
+    console.log(`4. ` + results);
+    if (results) {
+      return res.status(StatusCodes.UNAUTHORIZED).json({
+        message: 'info: 체크포인트4',
+      });
+    }
 
-  // 2. 신청대상 테이블 데이터 추가
-  sql = `INSERT INTO user (Application_Target) VALUES (?)`;
-  values = [businessApply];
-  [results] = await conn.query(sql, values);
-  console.log(`2. ` + results);
-  if (results) {
-    return res.status(StatusCodes.UNAUTHORIZED).json({
-      message: 'info: 체크포인트2',
-    });
-  }
-
-  // 3. 사업업력 테이블 데이터 추가
-  sql = `INSERT INTO user (Business_Duration) VALUES (?)`;
-  values = [businessExperience];
-  [results] = await conn.query(sql, values);
-  console.log(`3. ` + results);
-  if (results) {
-    return res.status(StatusCodes.UNAUTHORIZED).json({
-      message: 'info: 체크포인트3',
-    });
-  }
-
-  // 4. 사업대상연령 테이블 데이터 추가
-  sql = `INSERT INTO user (Target_Age_For_Business) VALUES (?)`;
-  values = [String(businessTargetAge)];
-  [results] = await conn.query(sql, values);
-  console.log(`4. ` + results);
-  if (results) {
-    return res.status(StatusCodes.UNAUTHORIZED).json({
-      message: 'info: 체크포인트4',
-    });
-  }
-  return res.status(StatusCodes.OK).json(results[0]);
+  return res.status(StatusCodes.OK).json(results);
 };
