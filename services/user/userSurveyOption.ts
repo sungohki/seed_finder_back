aa8ebe23e7eddc9755324d152742d556b7bc1cfc
sungohki <sungohki@DESKTOP-4QPLQ55>
2024-08-28 19:42:19 +0900

    feat: Add test code

--- services/user/userSurveyOption.ts
+++ services/user/userSurveyOption.ts
@@ -8,14 +8,15 @@ import mariadb from 'mysql2/promise';
 import { connInfo } from '../../mariadb';
 import { ISurveyInfo } from './userSurveyInfo';
 
+interface ISurveyOption {
+  business_classification: Array<{ id: number; name: string }>;
+  application_target: Array<{ id: number; name: string }>;
+  support_region: Array<{ id: number; name: string }>;
+}
+
 export const userSurveyOption = async (req: Request, res: Response) => {
-  const decodedUserAccount = verifyAccessToken(req, res);
-  if (decodedUserAccount === null)
-    return res.status(StatusCodes.UNAUTHORIZED).json({
-      message: 'info: 토큰 인식 불가',
-    });
   const conn = await mariadb.createConnection(connInfo);
-  let resValue = {} as ISurveyInfo;
+  let resValue = {} as ISurveyOption;
   let sql: string, results: mariadb.QueryResult;
 
   // 1. 지원사업분류
@@ -25,6 +26,10 @@ export const userSurveyOption = async (req: Request, res: Response) => {
   [results] = await conn.query(sql);
   console.log('1.');
   console.log(results);
+  resValue.business_classification = results as Array<{
+    id: number;
+    name: string;
+  }>;
 
   // 2. 신청대상
   sql = `
@@ -33,14 +38,22 @@ export const userSurveyOption = async (req: Request, res: Response) => {
   [results] = await conn.query(sql);
   console.log('2.');
   console.log(results);
+  resValue.application_target = results as Array<{
+    id: number;
+    name: string;
+  }>;
 
   // 3. 지역
   sql = `
     SELECT * FROM Support_Region
   `;
   [results] = await conn.query(sql);
-  console.log('1.');
+  console.log('3.');
   console.log(results);
+  resValue.support_region = results as Array<{
+    id: number;
+    name: string;
+  }>;
 
   return res.status(StatusCodes.OK).json({
     request: '설문 옵션 불러오기',
