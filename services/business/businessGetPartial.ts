b1585ed126943763df5df1c2767447e3ce50058c
sungohki <sokim1201@naver.com>
2024-09-02 13:38:55 +0900

    feat: Add test code

--- services/business/businessGetPartial.ts
+++ services/business/businessGetPartial.ts
@@ -18,16 +18,31 @@ export const businessGetPartial = async (req: Request, res: Response) => {
   console.log(userInfo);
 
   // 2. DB 연결
-  // const conn = await mariadb.createConnection(connInfo);
-  // let sql: string, values: Array<string | number> | null;
-  // let resValue;
+  const conn = await mariadb.createConnection(connInfo);
+
+  const { ex } = req.body as { ex: Array<number> };
+  let sql: string;
+  let values;
+  let results;
+
+  sql = `
+    select 
+      announcement_id
+    from 
+      Announcement_Application_Target
+		where 
+      application_target_id in [?]`;
+  values = [ex];
+  console.log(values);
+  [results] = await conn.query(sql);
+  console.log(results);
 
   return res.status(StatusCodes.OK).json({
-    ...userInfo,
+    // ...userInfo,
+    ...results,
   });
 
   // return res.status(StatusCodes.OK).json({
   //   request: '추천사업조회',
-  //   response: resValue,
   // });
 };
