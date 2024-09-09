e3a44a3c4a66233574ec43e5ab25c9d3e74c9da0
sungohki <sokim1201@naver.com>
2024-09-09 15:21:26 +0900

    feat: Fix response err

--- services/business/businessGetDetail.ts
+++ services/business/businessGetDetail.ts
@@ -5,31 +5,32 @@ import { StatusCodes } from 'http-status-codes';
 // import local module
 import mariadb from 'mysql2/promise';
 import { connection as conn } from '../../mariadb';
-import { ISurveyInfo } from '../user';
-import { verifyAccessToken } from '../common';
 
 export const businessGetDetail = async (req: Request, res: Response) => {
   const { businessId } = req.params;
   let sql, resValue;
   const values = [businessId];
 
-  sql = `
-    SELECT * FROM Announcment WHERE id = ?
-  `;
+  try{
+	  sql = `
+	    SELECT * FROM Announcement WHERE id = ?
+	  `;
 
-  conn.query(sql, values, (err, results) => {
-    if (err) {
-      console.log(err);
-      return res.status(StatusCodes.BAD_REQUEST).end();
-    }
-    resValue = Object(results);
-    return res.status(StatusCodes.OK).json({
-      ...resValue,
-    });
-  });
-
-  return res.status(StatusCodes.NO_CONTENT).json({
-    request: '사업 상세 조회',
-    response: '데이터베이스 결과 수신 오류',
-  });
+	  conn.query(sql, values, (err, results) => {
+	    if (err) {
+	      console.log(err);
+	      return res.status(StatusCodes.BAD_REQUEST).end();
+	    }
+	    resValue = Object.values(results)[0];
+	    // console.log(resValue);
+	    return res.status(StatusCodes.OK).json({
+	      ...resValue,
+	    });
+	  });
+  }catch(e) {
+	  return res.status(StatusCodes.NO_CONTENT).json({
+	    request: '사업 상세 조회',
+	    response: '데이터베이스 결과 수신 오류',
+	  });
+  }
 };
