75a228315923b6a37754d29e0aff2d267565848d
sungohki <sokim1201@naver.com>
2024-09-12 00:58:49 +0900

    feat: Export codes

--- services/business/businessGetAll.ts
+++ services/business/businessGetAll.ts
@@ -5,7 +5,7 @@ import { StatusCodes } from 'http-status-codes';
 // import local module
 import { connection as conn } from '../../mariadb';
 
-interface IBusinessPreview {
+export interface IBusinessPreview {
   id: number;
   integrated_project_name: string;
   business_classification_id: number;
@@ -13,7 +13,7 @@ interface IBusinessPreview {
   end_date: string;
 }
 
-const businessToCalender = (businessData: Array<IBusinessPreview>) => {
+export const businessToCalender = (businessData: Array<IBusinessPreview>) => {
   let businessCalenderMap = new Map<string, Array<IBusinessPreview>>();
   const businessCalenderJson = {} as {
     [date: string]: Array<IBusinessPreview>;
@@ -48,7 +48,6 @@ export const businessGetAll = (req: Request, res: Response) => {
   `;
   // 아이디 통합공고사업명 지원사업분류 공고접수일시(시작 종료)
   const reqQuery = req.query as { page: string; limit: string };
-  let resValue;
   let values: number[] | undefined;
 
   console.log(reqQuery); // query 체크
@@ -63,9 +62,10 @@ export const businessGetAll = (req: Request, res: Response) => {
         console.log(err);
         return res.status(StatusCodes.BAD_REQUEST).end();
       }
-      resValue = Object.values(results) as Array<IBusinessPreview>;
       return res.status(StatusCodes.OK).json({
-        ...businessToCalender(resValue),
+        ...businessToCalender(
+          Object.values(results) as Array<IBusinessPreview>
+        ),
       });
     });
   } catch (e) {
