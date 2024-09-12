43be3e60017c8214fd4e0ce45bdec3e001f1f36f
sungohki <sokim1201@naver.com>
2024-09-12 15:22:13 +0900

    feat: Fix swagger doucment

--- swagger/favorite.swagger.ts
+++ swagger/favorite.swagger.ts
@@ -3,26 +3,30 @@
  *  tags:
  *    name: Favorite
  *    description: API to manage User's personal Favorite Business Datas.
+ *  components:
+ *    securitySchemes:
+ *      bearerAuth:
+ *        type: http
+ *        scheme: bearer
+ *        bearerFormat: JWT
  */
 
 /**
  * @swagger
  * /favorite/add/{businessId}:
- *   create:
+ *   post:
  *     summary: Create user-business reltation data
- *     security:
- *       - Authorization: []
- *     tags:
- *       - Board
- *     parameters:
- *       - in: header
- *       name: Authorization
- *       schema:
- *         type: string
- *       description: 우측 상단 좌물쇠 버튼을 눌러 값을 넣은 후 테스트 해주세요! 아래에는 값을 넣지 말고 테스트 해주세요!!
  *     tags: [Favorite]
+ *     parameters:
+ *       - name: businessId
+ *         in: path
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of the business to add to favorites
  *     requestBody:
- *       required: false
+ *       description: No body content is required.
+ *       content: {}
  *     responses:
  *       200:
  *         description: Success to create user-business data
@@ -37,6 +41,8 @@
  *                   description: Number of db's affected row
  *       400:
  *         description: Bad Request
+ *     security:
+ *       - bearerAuth: []
  *
  * /favorite/delete/{businessId}:
  *   delete:
