c7921c8af0ac200d2ace3022a91a11d3445f16f3
sungohki <sokim1201@naver.com>
2024-10-14 20:27:10 +0900

    feat: Refactoring codes

--- services/docs/documentCreateOne.ts
+++ services/docs/documentCreateOne.ts
@@ -20,31 +20,38 @@ import { IDocumentRequest, IDocumentGuide } from '.';
 export const documentCreateOne = async (req: Request, res: Response) => {
   const decodedUserAccount = accessTokenVerify(req, res);
   if (decodedUserAccount === null) return;
-  const doucmentRequest = req.body as IDocumentRequest;
+  const documentRequest = req.body as IDocumentRequest;
   const conn = await mariadb.createConnection(connInfo);
-  const sql = `
-    SELECT *
-    FROM Guide
-    WHERE document_topic_id = 
-    (SELECT id FROM Document_Topic WHERE numbering_id = ?);
-  `;
-  let values = [doucmentRequest.numberingId];
 
   try {
+    let sql = `
+      SELECT id FROM Document_Topic WHERE numbering_id = ?
+    `;
+    let values: Array<string> = [documentRequest.numberingId];
+    // 1. numbering_id 전처리
     let [results] = await conn.query(sql, values);
-    res.status(StatusCodes.OK).end();
+
+    // 2. 해당되는 guide 읽어오기
+    sql = `
+      SELECT *
+      FROM Guide
+      WHERE document_topic_id = ?
+    `;
+    values = [];
+    [results] = await conn.query(sql, values);
     const openAiAnswer: Array<String | undefined> = [];
     const temp: Array<IDocumentGuide> = [];
     for (const item of results as Array<IDocumentGuide>)
       temp.push(convertKeysToCamelCase(item));
 
+    res.status(StatusCodes.OK).end();
     for (const item of temp) {
-      const ret = await generateMessage(item.id, doucmentRequest.message);
+      const ret = await generateMessage(item.id, documentRequest.message);
       openAiAnswer.push(ret);
     }
     await documentInsert(
       decodedUserAccount.id,
-      doucmentRequest,
+      documentRequest,
       temp[0].id,
       openAiAnswer
     );
@@ -55,51 +62,47 @@ export const documentCreateOne = async (req: Request, res: Response) => {
     const testToken =
       'fIY1yA8nRN6iLi8GDfos5h:APA91bHG2LlehZGIC1TK0yLAzAmwE0CW5EtGAxeJR9WBQaKuoyEQwXu4UUEWsrdjgzXtKFnd19ZCvSC-xd15lN8jOVIaeEEaHjGLzskNlizgnR54ejvTpiBmiRPW3PeNYf6G5foDn3RJ';
 
-    console.log(doucmentRequest);
-    // if (doucmentRequest.deviceToken) {}
+    console.log(documentRequest);
+    // if (documentRequest.deviceToken) {
+    // }
     const messageRequest: IMessageRequest = {
-      title: doucmentRequest.title,
-      body: doucmentRequest.message,
+      title: documentRequest.title,
+      body: documentRequest.message,
       data: {
-        documentId: doucmentRequest.numberingId,
+        documentId: documentRequest.numberingId,
       },
       deviceToken: testToken,
     };
     // deviceToken: doucmentRequest.deviceToken,
-    sendFCM(messageRequest);
+    return sendFCM(messageRequest);
   } catch (e) {
     console.error(e);
-    return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(e);
+    return res.status(StatusCodes.BAD_REQUEST).json(e);
   } finally {
     await conn.end();
   }
 };
 
+interface ITraining {
+  messages: Array<ChatCompletionMessageParam>;
+}
+
 export const generateMessage = async (num: number, param: string) => {
   try {
     const messages: Array<ChatCompletionMessageParam> = [];
-
     const history = await readFile('./data/training_data.jsonl', 'utf-8');
     const lines = history.split('\n').filter((line) => line.trim() !== '');
-    if (num >= lines.length) {
+    if (num >= lines.length)
       throw new Error(`info: ${num})에 맞는 항목이 없습니다.`);
-    }
-    lines.forEach((line)=>{
-      const parsedLine = JSON.parse(line);
-      if(parsedLine.messages[0].content.includes(`${num}번 항목`)){
-        parsedLine.messages.forEach(
-          (a: ChatCompletionMessageParam) => {
-            messages.push(a);
-          }
-        );
+    lines.forEach((line) => {
+      const parsedLine: ITraining = JSON.parse(line);
+      const parsedLineContent = parsedLine.messages[0].content as string;
+      if (parsedLineContent?.includes(`${num}번 항목`)) {
+        parsedLine.messages.forEach((messageParam) => {
+          messages.push(messageParam);
+        });
       }
-    })
-    // const parsedLine = JSON.parse(lines[num]);
-    // const parsedLineContent = parsedLine.messages.forEach(
-    //   (a: ChatCompletionMessageParam) => {
-    //     messages.push(a);
-    //   }
-    // );
+    });
 
     const data = await readFile('./data/guidelines.json', 'utf8');
     const guidelines = JSON.parse(data);
@@ -117,7 +120,7 @@ export const generateMessage = async (num: number, param: string) => {
       messages: messages,
       model: process.env.FINE_TUNING_MODEL as string,
     });
-    return response.choices[0].message.content?.replace("**","") as string;
+    return response.choices[0].message.content?.replace('**', '') as string;
   } catch (e) {
     console.error(e);
   }
