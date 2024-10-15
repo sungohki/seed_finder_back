90cff6335990d4b4b03ef4ff84de5e31a7896a4e
sungohki <sokim1201@naver.com>
2024-10-15 17:46:36 +0900

    feat: Modify testFCM

--- services/test/testSendFCM.ts
+++ services/test/testSendFCM.ts
@@ -6,21 +6,31 @@ dotenv.config();
 
 // Import local module
 import { IMessageRequest, sendFCM } from '../common/sendFCM';
+import { IDocumentRequest } from '../docs';
 
-const testToken =
-  'fIY1yA8nRN6iLi8GDfos5h:APA91bHG2LlehZGIC1TK0yLAzAmwE0CW5EtGAxeJR9WBQaKuoyEQwXu4UUEWsrdjgzXtKFnd19ZCvSC-xd15lN8jOVIaeEEaHjGLzskNlizgnR54ejvTpiBmiRPW3PeNYf6G5foDn3RJ';
+// const testToken =
+//   'fIY1yA8nRN6iLi8GDfos5h:APA91bHG2LlehZGIC1TK0yLAzAmwE0CW5EtGAxeJR9WBQaKuoyEQwXu4UUEWsrdjgzXtKFnd19ZCvSC-xd15lN8jOVIaeEEaHjGLzskNlizgnR54ejvTpiBmiRPW3PeNYf6G5foDn3RJ';
 
-const testRequest: IMessageRequest = {
-  title: 'test',
-  body: 'Hello, World!',
-  data: {
-    documentId: '1',
-  },
-  deviceToken: testToken,
-};
+// const testRequest: IMessageRequest = {
+//   title: 'test',
+//   body: 'Hello, World!',
+//   data: {
+//     documentId: '1',
+//   },
+//   deviceToken: testToken,
+// };
 
 export const testSendFCM = async (req: Request, res: Response) => {
-  const result = await sendFCM(testRequest);
+  const reqBody = req.body as IDocumentRequest;
+  const temp: IMessageRequest = {
+    title: reqBody.title,
+    body: reqBody.message,
+    data: {
+      documentId: reqBody.numberingId,
+    },
+    deviceToken: reqBody.deviceToken,
+  };
+  const result = await sendFCM(temp);
   if (result)
     return res.status(StatusCodes.OK).json({
       message: 'success',
