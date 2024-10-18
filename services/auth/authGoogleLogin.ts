7f474fc89db06ec32a4ab6e4e646ffd0b957dc46
KangJuHyeong <kkjjhh7935@gmail.com>
2024-10-18 17:56:27 +0900

    feat: change authgooglelogin

--- services/auth/authGoogleLogin.ts
+++ services/auth/authGoogleLogin.ts
@@ -1,10 +1,33 @@
 // Import node module
 import { Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
+import mariadb, { QueryResult, RowDataPacket } from 'mysql2/promise';
+import { connInfo } from '../../config/mariadb';
+import { IAuthUser, accessTokenGenerate } from '../common';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 import axios from 'axios'; // Axios for making HTTP requests
 
+interface IGoogleUser {
+    sub: string,
+    name: string,
+    given_name: string,
+    family_name: string,
+    picture: string,
+    email: string
+  }
 export const authGoogleLogin = async (req: Request, res: Response) => {
+    
+
     const {  googleAccessToken } = req.body; // 클라이언트에서 받은 액세스 토큰
+    const accessPrivateKey = process.env.ACCESS_PRIVATE_KEY;
+    const refreshPrivateKey = process.env.REFRESH_PRIVATE_KEY;
+    if (!accessPrivateKey || !refreshPrivateKey) {
+      console.error('Info: PRIVATE_KEY가 환경변수로 지정되어있지 않음.');
+      return res.status(StatusCodes.INTERNAL_SERVER_ERROR).end();
+    }
+    const conn = await mariadb.createConnection(connInfo);
 
     if (!googleAccessToken) {
         return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Access token is required' });
@@ -19,16 +42,63 @@ export const authGoogleLogin = async (req: Request, res: Response) => {
         });
 
         // 2. 사용자 정보 추출
-        const userData = response.data; // 여기서 사용자 정보를 가져옵니다.
+        const userData: IGoogleUser = response.data as IGoogleUser; // 여기서 사용자 정보를 가져옵니다.
         console.log(userData)
 
-        // 3. 필요한 경우 사용자 정보를 데이터베이스에 저장하거나 처리
-        // 예시: const user = await User.findOne({ googleId: userData.sub });
+        // 2. 해당 유저가 존재하는 지 확인
+        let sql = `SELECT * FROM User WHERE uuid = ? LIMIT 1`;
+        let values = [userData.sub];
+        let [results] = await conn.query(sql, values);
+        const loginUser = (results as Array<{ id: number }>)[0];
 
-        // 4. 응답 반환 (예시로 사용자 정보를 반환)
+        // 2-1. 없는 존재인 경우 회원 생성
+        if (!loginUser) {
+        sql = `
+            INSERT INTO 
+            User 
+            (user_uuid, user_name, user_email) 
+            VALUES 
+            (?, ?, ?)`;
+        values = [
+            userData.sub,
+            userData.name,
+            userData.email
+        ];
+        [results] = await conn.query(sql, values);
+        }
+
+        // 3-1. jwt 액세스 토큰 발행
+        const accessTokenInfo: IAuthUser = {
+        id: loginUser.id,
+        uuid: userData.sub,
+        userName: userData.name,
+        };
+        const accessTokenOption: jwt.SignOptions = {
+        expiresIn: '1h',
+        issuer: 'sungohki',
+        };
+        const instanceAccessToken = accessTokenGenerate(
+        accessTokenInfo,
+        accessPrivateKey,
+        accessTokenOption
+        );
+        console.log('Info: 토큰 발행');
+
+        // 3-2. jwt 리프레시 토큰 발행
+        const refreshTokenOption: jwt.SignOptions = {
+        expiresIn: '1d',
+        issuer: 'sungohki',
+        };
+        const instanceRefreshToken = accessTokenGenerate(
+        accessTokenInfo,
+        refreshPrivateKey,
+        refreshTokenOption
+        );
+
         return res.status(StatusCodes.OK).json({
-            message: 'User authenticated successfully',
-            user: userData // 또는 필요한 사용자 데이터
+        uuid: userData.sub,
+        accessToken: instanceAccessToken,
+        refreshToken: instanceRefreshToken,
         });
 
     } catch (error) {
