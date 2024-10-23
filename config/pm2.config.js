e49633cc6888414501958d2969b49d4b2d6571e7
sungohki <sokim1201@naver.com>
2024-10-23 17:06:58 +0900

    feat: Modify pm2 config

--- config/pm2.config.js
+++ config/pm2.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'app',
-      script: './index.ts', // 실행할 TypeScript 파일
+      script: './dist/src/index.js', // 실행할 TypeScript 파일
       interpreter: 'ts-node', // ts-node로 TypeScript 실행
       watch: true, // 파일이 변경되면 자동으로 다시 시작
       exec_mode: 'cluster',
