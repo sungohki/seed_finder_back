930d2d4d2ef2a145e30cb7a99c8daa3fb286655d
sungohki <sokim1201@naver.com>
2024-11-01 17:40:05 +0900

    feat: Fix data directory path

--- src/services/docs/documentCreateOne.ts
+++ src/services/docs/documentCreateOne.ts
@@ -93,7 +93,10 @@ interface ITraining {
 export const generateMessage = async (num: number, param: string) => {
   try {
     const messages: Array<ChatCompletionMessageParam> = [];
-    const history = await readFile('./data/training_data.jsonl', 'utf-8');
+    const history = await readFile(
+      '../../../data/training_data.jsonl',
+      'utf-8'
+    );
     const lines = history.split('\n').filter((line) => line.trim() !== '');
     if (num >= lines.length)
       throw new Error(`info: ${num})에 맞는 항목이 없습니다.`);
@@ -107,7 +110,7 @@ export const generateMessage = async (num: number, param: string) => {
       }
     });
 
-    const data = await readFile('./data/guidelines.json', 'utf8');
+    const data = await readFile('../../../data/guidelines.json', 'utf8');
     const guidelines = JSON.parse(data);
     const messageContent = guidelines[num];
     if (!messageContent)
