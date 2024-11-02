6e14b94692842460ec9fa229fd490b680d4f0568
sungohki <sokim1201@naver.com>
2024-11-02 19:22:03 +0900

    feat: Modify req.body info to query

--- src/services/docs/documentGetGuide.ts
+++ src/services/docs/documentGetGuide.ts
@@ -12,19 +12,17 @@ interface IGuideContent {
 }
 
 export const documentGetGuide = (req: Request, res: Response) => {
-  const { numberingId } = req.body;
+  const { id } = req.query;
   const sql = `
     SELECT guide_content FROM Guide WHERE document_topic_id = 
       (SELECT id FROM Document_Topic WHERE numbering_id = ?) LIMIT 1`;
-  const values = [numberingId];
+  const values = [id];
 
   conn.query(sql, values, (err, results) => {
     if (queryErrorChecker(err, res)) return;
     const guideLines = convertKeysToCamelCase(
       Object.values(results)[0]
     ) as IGuideContent;
-    return res.status(StatusCodes.OK).json({
-      guideLines: guideLines.guideContent,
-    });
+    return res.status(StatusCodes.OK).send(guideLines);
   });
 };
