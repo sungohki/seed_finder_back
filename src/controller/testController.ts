a1ea22efb1bef990fdb73ce5f59b41f097ca1ca4
sungohki <sokim1201@naver.com>
2024-11-07 09:30:46 +0900

    feat: Remove test route

--- src/controller/testController.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from 'express';
-import * as TS from '../services/test';
-
-export const testSendFCMHander = (req: Request, res: Response) => {
-  return TS.testSendFCM(req, res);
-};
